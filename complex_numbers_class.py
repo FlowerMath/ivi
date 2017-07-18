e178c0f453aa4757af59846b130a8f36a8c5dbf1
Nadya <n.gorlova@yandex.ru>
2017-07-19 01:03:25 +0300

    complex_numbers_class

--- complex_numbers_class.py
+++ complex_numbers_class.py
@@ -3,10 +3,16 @@ class ComplexNumber:
         self.real = r_part
         self.im = im_part
 
+    def __eq__(self, other):
+        if self.real == other.real and self.im == other.im:
+            return True
+        else:
+            return False
+
     def printing(self):
         print(self.real, '+', self.im, 'i')
 
-    def module(self):
+    def module2(self):
         return self.real ** 2 + self.im ** 2
 
     def plus(self, other):
@@ -23,9 +29,9 @@ class ComplexNumber:
                              self.im * other.real + self.real * other.im)
 
     def division(self, other):
-        mod = other.module()
+        mod = other.module2()
         if mod != 0:
             return ComplexNumber((self.real * other.real + self.im * other.im) / mod,
                                  (self.im * other.real - other.im * self.real) / mod)
         else:
-            return ComplexNumber('none (module = 0)', 'none (module = 0)')
+            return ComplexNumber('none (mod = 0)', 'none')
